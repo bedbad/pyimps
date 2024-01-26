0b4c11c12fcea62ba99d16289ff6cd8816e9a9d4
bedbad <antonyuk@bu.edu>
2024-01-26 05:45:50 +0200

    pip package

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyimports',
-    version='0.0.1',
+    name='pyimps',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=[
         'requests',
@@ -11,13 +11,13 @@ setup(
     author='bedbad',
     author_email='antonyuk@bu.edu',
     description='Unconfuse your Py imports',
-    long_description='''Resolve your Py imports - see all'''
-                     '''Where are do those that you use sit, where'''
-                     '''those that left, should be, see all dependencies,'''
-                     '''and the way the were imported and defined - makes you adhere to'''
-                     '''well defined Python project structure, and potentially automate build''',
+    long_description='''Resolve your Py imports - see entire listing \r\n'''
+                     '''Where all tha you have sit; \r\n'''
+                     '''Where all tha you don't, should be; \r\n'''
+                     '''See all transitives the way the were imported and defined. \r\n'''
+                     '''All of it - right in your terminal \r\n''',
     long_description_content_type='text/markdown',
-    url='https://github.com/bedbad/justpyplot',
+    url='https://github.com/bedbad/pyimps',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
